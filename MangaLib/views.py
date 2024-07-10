0fb69db2300fd50cf599487e0fb4efdaec4b0d98
C++ torturer <8353207@gmail.com>
2024-07-11 01:50:04 +0300

    minor changes

--- MangaLib/views.py
+++ MangaLib/views.py
@@ -22,15 +22,15 @@ class MangaListView(generics.ListAPIView):  # GET все тайтлы
     serializer_class = MangaSerializer
 
 
-class MangaCreateView(APIView):  # POST add manga
+class MangaCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         serializer = MangaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MangaDetailView(generics.RetrieveAPIView): # GET конкретный тайтл
